e4ed260c0d0836a1512bf8d4b6abf8676a567694
Florian Eble <florian.eble@cern.ch>
2022-08-24 17:29:21 +0200

    Add setup.sh and cmsenv.sh scripts (README already committed before by mistake)

---
+++ setup.py
@@ -0,0 +1,4 @@
+git clone ssh://git@gitlab.cern.ch:7999/cmsoms/oms-api-client.git
+mv oms-api-client/omsapi .
+rm -rf oms-api-client
+source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc8-opt/setup.sh
