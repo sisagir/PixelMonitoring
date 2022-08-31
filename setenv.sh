be8a970a9d3cb1ed6505a2ecedc0e84dacdc41c2
Florian Eble <florian.eble@cern.ch>
2022-08-31 10:34:14 +0200

    Create PIXEL_MONITORING_DIR env variable for convevience and add root repo directory to PYTHONPATH

--- setenv.sh
+++ setenv.sh
@@ -1,1 +1,4 @@
-source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc8-opt/setup.sh
+export LCG_RELEASE_DIR=/cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc8-opt
+export PIXEL_MONITORING_DIR=${PWD}
+export PYTHONPATH=${PYTHONPATH}:${PWD}
+source ${LCG_RELEASE_DIR}/setup.sh
