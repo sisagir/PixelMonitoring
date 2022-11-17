8df2b6f990dc6bbaf64a63b929fd857e64417867
Florian Eble <florian.eble@cern.ch>
2022-11-17 10:43:14 +0100

    Creating BPixModule, FPixModule and ReadoutGroup classes and updating/adding method to get average fluence / azimuthal angle over a readout group

--- src/utils/Module.py
+++ src/utils/Module.py
@@ -1,39 +1,154 @@
 import collections
+from config.geometry.BPix.name_position_map import name_position_map_bpix
+from config.geometry.FPix.name_position_map import name_position_map_fpix
+from config.geometry.BPix.module_size import module_size
 
 
-class Module:
-    def __init__(self,name):
-        self.module_position = collections.namedtuple('module_position', 'R0 L0 Z0')
-        self.module_size = collections.namedtuple('module_size', 'dR dL dZ')
-        self.position = self.module_position(0,0,0)
-        self.size = self.module_size(0,0,0)
+PositionCoordinates = collections.namedtuple('Coordinate', ["r", "phi", "z"])
+SizeCoordinates = collections.namedtuple('Coordinate', ["r", "rphi", "z"])
+
+
+class BPixModule:
+    """Class representing a BPix module."""
+
+    def __init__(self, name):
+        """BPix module constructor.
+        
+        Args:
+            name (str): Module name, e.g. BPix_BmI_SEC4_LYR1_LDR3F_MOD1
+        """
+
         self.name = name
-        ppXS = 79.1
-        self.conversionFactor=ppXS*10e-15/10e-27
-    def get_name(self):
-        return self.name
-    def getVolume(self):
-        return self.size.dR*self.size.dL*self.size.dZ
-    def getElemVolume(self,dr):
-        return self.dr*self.size.dL*self.size.dZ
-    def setPosition(self,R0,L0,Z0):
-        self.position = self.module_position(R0,L0,Z0)
-    def setSize(self,dR,dL,dZ):
-        self.size = self.module_size(dR,dL,dZ)
-    def setElemVolume(self,dr):
-        self.dr=dr
-    def getAverageFluence(self,fluence_th2f,lumi):
-        R0=self.position.R0-self.size.dR/2.+self.dr/2.
-        R=R0
-        phi_eq=0
-        while R<self.position.R0+self.size.dR/2.:
-            # print "fluence(%s) = %s"%(R,fluence_th2f.GetBinContent(fluence_th2f.GetXaxis().FindBin(R),fluence_th2f.GetYaxis().FindBin(self.position.Z0)))
-            phi_eq+=fluence_th2f.GetBinContent(fluence_th2f.GetXaxis().FindBin(R),fluence_th2f.GetYaxis().FindBin(self.position.Z0))
-            R+=self.dr
-        return self.dr/self.size.dR*phi_eq*self.conversionFactor*lumi
+        if name not in name_position_map_bpix:
+            raise ValueError("Invalid BPix module name {name}")
+        position = name_position_map_bpix[self.name]
+        self.position = PositionCoordinates(*position)
+        self.size = SizeCoordinates(*module_size)
 
-def getAverageFluenceSum(list_of_modules,fluence_th2f,lumi):
-	fluence = 0
-	for m in list_of_modules:
-		fluence+=m.getAverageFluence(fluence_th2f,lumi)
-	return fluence
+    def getAverageFluence(self, fluence_field, lumi, pp_cross_section, dz=0.1):
+        """
+        Args:
+            fluence_field (ROOT.TH2F): Fluence field, in n_eq.cm-2
+            lumi (float): Integrated luminosity in fb-1
+            pp_cross_section (float): proton-proton total cross-section in mb
+            dz (float): axial integration element in cm
+    
+        Returns:
+            float: Fluence for the module, in n_eq.cm-2
+        """
+
+        lumi *= 1e-15  # convert from /fb to /b
+        pp_cross_section *= 10**27  # convert from mb to cm2
+
+        z = self.position.z - self.size.r/2. + dz/2.
+        phi_eq = 0
+        while z < self.position.z + self.size.z/2.:
+            phi_eq += fluence_field.GetBinContent(
+                fluence_field.GetXaxis().FindBin(self.position.r),
+                fluence_field.GetYaxis().FindBin(z)
+            )
+            z += dz
+
+        return (dz/self.size.z) * phi_eq * lumi * pp_cross_section
+
+
+class FPixModule:
+    """Class representing a FPix module."""
+
+    def __init__(self, name):
+        """FPix module constructor.
+        
+        Args:
+            name (str): Module name, e.g. FPix_BpI_D1_BLD6_PNL2_RNG1
+        """
+
+        self.name = name
+        if name not in name_position_map_fpix:
+            raise ValueError("Invalid FPix module name {name}")
+        position = name_position_map_fpix[name]
+        self.position = PositionCoordinates(*position)
+        self.size = SizeCoordinates(*module_size)
+
+    def getAverageFluence(self, fluence_field, lumi, pp_cross_section, dr=0.1):
+        """
+        Args:
+            fluence_field (ROOT.TH2F): Fluence field, in n_eq.cm-2
+            lumi (float): Integrated luminosity in fb-1
+            pp_cross_section (float): proton-proton total cross-section in mb
+            dr (float): radial integration element in cm
+    
+        Returns:
+            float: Fluence for the module, in n_eq.cm-2
+        """
+
+        lumi *= 1e-15  # convert from /fb to /b
+        pp_cross_section *= 10**27  # convert from mb to cm2
+
+        r = self.position.r - self.size.r/2. + dr/2.
+        phi_eq = 0
+        while r < self.position.r + self.size.r/2.:
+            phi_eq += fluence_field.GetBinContent(
+                fluence_field.GetXaxis().FindBin(r),
+                fluence_field.GetYaxis().FindBin(self.position.z)
+            )
+            r += dr
+
+        return (dr/self.size.r) * phi_eq * lumi * pp_cross_section
+
+
+class ReadoutGroup:
+    """Class representing a BPix or FPix readout group."""
+
+    def __init__(self, name):
+        """ReadoutGroup constructor.
+        
+        Args:
+            name (str): Read out group name, a.k.a. sector name, e.g. BPix_BmI_SEC4_LYR1
+        """
+
+        self.name = name
+        sub_system = name.split("_")[0]
+        self.list_of_modules = []
+
+        if sub_system not in ["FPix", "BPix"]:
+            raise ValueError(f"Invalid read out group {name}")
+
+        if sub_system == "BPix":
+            name_position_map = name_position_map_bpix
+            for module_name in name_position_map.keys():
+                if module_name.startswith(name): 
+                    self.list_of_modules.append(BPixModule(module_name))
+        else:
+            name_position_map = name_position_map_fpix
+            raise NotImplementedError
+        
+        if len(self.list_of_modules) == 0:
+            raise ValueError(f"Invalid read out group name {name}")
+
+    def getAverageAzimuthalAngle(self):
+        """Return average azimuthal angle over the modules in this readout group."""
+
+        phi_average = 0.
+        for module in self.list_of_modules:
+            phi_average += module.position.phi
+        phi_average /= len(self.list_of_modules)
+
+        return phi_average
+
+    def getAverageFluence(self, fluence_field, lumi, pp_cross_section, dx=0.1):
+        """Return average fluence over the modules in this readout group.
+
+        Args:
+            fluence_field (ROOT.TH2F): Fluence field, in n_eq.cm-2
+            lumi (float): Integrated luminosity in fb-1
+            pp_cross_section (float): proton-proton total cross-section in mb
+            dx (float): infinitesimal integration element in cm
+    
+        Returns:
+            float: Fluence for the module, in n_eq.cm-2
+        """
+
+        fluence = 0
+        for module in self.list_of_modules:
+            fluence += module.getAverageFluence(fluence_field, lumi, pp_cross_section, dx)
+        return fluence
