00cff5c2477b78a76ed4c4d921a67155b2e37ede
Florian Eble <florian.eble@cern.ch>
2023-01-19 16:35:27 +0100

    Adding I/O, leakage currents and other useful flags

--- src/utils/parserUtils.py
+++ src/utils/parserUtils.py
@@ -10,7 +10,7 @@ class ArgumentParser(argparse.ArgumentParser):
 
     def add_input_fills_file_flag(self):
         self.add_argument(
-            "-i", "--input_fills_file_name",
+            "-f", "--input_fills_file_name",
             help="Fills file, default=%(default)s",
             required=False,
             default="data/fills_info/fills.csv",
@@ -37,10 +37,24 @@ class ArgumentParser(argparse.ArgumentParser):
             required=last_fill_required,
         )
     
+    def add_luminosity_input_flag(self):
+        self.add_argument(
+            "-l", "--input_lumi_file_name",
+            help="Input luminosity file, default=%(default)s",
+            default="data/luminosity/integrated_luminosity_per_fill.csv",
+        )
+
     def add_currents_input_directory_flag(self, default_directory):
         self.add_argument(
             "-c", "--input_currents_directory",
             help="Input currents directory, default=%(default)s",
+            default=default_directory,
+        )
+
+    def add_temperatures_input_directory_flag(self, default_directory):
+        self.add_argument(
+            "-t", "--input_temperatures_directory",
+            help="Input temperature directory, default=%(default)s",
             default=default_directory,
         )
 
@@ -69,6 +83,24 @@ class ArgumentParser(argparse.ArgumentParser):
             type=int,
             choices=[1, 2, 3, 4],
             default=default,
+            required=False,
+        )
+
+    def add_layers_flag(self, default="1,2,3,4"):
+        self.add_argument(
+            "-layers", "--layers",
+            help="Layer",
+            default=default,
+            required=False,
+        )
+
+    def add_measurement_delay_flag(self, quantity):
+        self.add_argument(
+            "-delay", "--measurement_delay",
+            help=f"Time in s after begining of stable beam after which to "
+                 f"measure the {quantity}, default=%(default)s s",
+            default=1200,
+            type=int,
             required=False,
         )
 
@@ -105,3 +137,33 @@ class ArgumentParser(argparse.ArgumentParser):
             **args2,
         )
  
+    def add_leakage_current_flags(self):
+        self.add_input_fills_file_flag()
+        self.add_bad_fills_file_flag()
+        self.add_luminosity_input_flag()
+        self.add_currents_input_directory_flag(default_directory="data/currents/processed")
+        self.add_temperatures_input_directory_flag(default_directory="data/temperatures/readout_group")
+        self.add_output_directory_flag(default_directory="plots/currents")
+        self.add_sub_system_flag()
+        self.add_argument(
+            "-tref", "--target_temperature",
+            help="Temperature target for leakage current rescaling, in Celsius, default=%(default)s",
+            default=None,
+        )
+        self.add_argument(
+            "-normvol", "--normalize_to_volume",
+            help="Normalize leakage current to sensor volume. Cannot be used with -normroc.",
+            action="store_true",
+        )
+        self.add_argument(
+            "-normroc", "--normalize_to_number_of_rocs",
+            help="Normalize leakage current to number of rocs.  Cannot be used with -normvol.",
+            action="store_true",
+        )
+    
+
+def sanity_checks_leakage_current_flags(args):
+    if ((args.normalize_to_volume or args.normalize_to_number_of_rocs)
+         and (args.normalize_to_volume and args.normalize_to_number_of_rocs)):
+         print("Error: normalize_to_volume and normalize_to_number_of_rocs cannot be used at the same time!")
+         exit(1)
